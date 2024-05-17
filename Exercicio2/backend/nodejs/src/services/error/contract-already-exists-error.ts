06a34e5ed89116c78c4654abbc43408f8e4ce18f
Daniel Sampar <daniel_sampar@hotmail.com>
2024-05-17 19:18:26 -0300

    fix(contract): Fix ContractAlreadyExistsError name

--- Exercicio2/backend/nodejs/src/services/error/contract-already-exists-error.ts
+++ Exercicio2/backend/nodejs/src/services/error/contract-already-exists-error.ts
@@ -1,4 +1,4 @@
-export class ContractAlreadyExists extends Error {
+export class ContractAlreadyExistsError extends Error {
   constructor() {
     super(
       'A contract already exists, if you want to change it please use the update route.',

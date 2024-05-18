0a89b872ddfc9133fa14da2b49802fe5844d9e64
Daniel Sampar <daniel_sampar@hotmail.com>
2024-05-18 17:22:25 -0300

    config(frontend): Configure Chakra UI

--- Exercicio2/frontend/src/main.tsx
+++ Exercicio2/frontend/src/main.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+
+const colors = {
+  brand: {
+    blue: '#388ccc',
+    navyBlue: '#1a2933',
+  },
+}
+
+const theme = extendTheme({ colors })
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
 )

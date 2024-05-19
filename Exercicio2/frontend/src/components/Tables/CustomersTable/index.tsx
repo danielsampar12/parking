02caf3dda31e73edccb953dbed67620ac2c3dd7e
Daniel Sampar <daniel_sampar@hotmail.com>
2024-05-19 16:30:01 -0300

    chore: Change tables loading and error component

--- Exercicio2/frontend/src/components/Tables/CustomersTable/index.tsx
+++ Exercicio2/frontend/src/components/Tables/CustomersTable/index.tsx
@@ -1,4 +1,8 @@
 import {
+  Alert,
+  AlertDescription,
+  AlertIcon,
+  AlertTitle,
   Flex,
   IconButton,
   Spinner,
@@ -90,13 +94,35 @@ export function CustomersTable({
   if (isLoading) {
     return (
       <Flex flex={1} w="full" alignItems="center" justifyContent="center">
-        <Spinner size="2xl" />
+        <Spinner size="3xl" color="brand.blue" />
       </Flex>
     )
   }
 
-  if (!data || isError) return <></>
-
+  if (isError) {
+    return (
+      <Flex flex={1} w="full" alignItems="center" justifyContent="center">
+        <Alert
+          status="error"
+          variant="subtle"
+          flexDirection="column"
+          alignItems="center"
+          justifyContent="center"
+          textAlign="center"
+          height="200px"
+        >
+          <AlertIcon boxSize="40px" mr={0} />
+          <AlertTitle mt={4} mb={1} fontSize="lg">
+            Ocorreu um erro!
+          </AlertTitle>
+          <AlertDescription maxWidth="sm">
+            Ocorreu um erro ao buscar a informação. Verifique sua conexão à
+            internet. Caso o erro persista, contate o suporte.
+          </AlertDescription>
+        </Alert>
+      </Flex>
+    )
+  }
   return (
     <TableContainer w="full" h="full" pt="20px">
       <Table variant="simple">
